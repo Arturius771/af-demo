1cc90c212b66256515e9c2c812e3020437f60515
Artur Foden <fodena@tcd.ie>
2022-09-10 10:10:06 +0100

    added square button

--- src/app/buttons/square-button/square-button.component.ts
+++ src/app/buttons/square-button/square-button.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-square-button',
   templateUrl: './square-button.component.html',
-  styleUrls: ['./square-button.component.less']
+  styleUrls: ['./square-button.component.less'],
 })
 export class SquareButtonComponent implements OnInit {
+  @Input()
+  buttonText: string = 'Click';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
