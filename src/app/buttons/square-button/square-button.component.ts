e6a5a1803f2e1a6655233ecbd8e2b984b3ebd496
Artur Foden <fodena@tcd.ie>
2022-09-10 22:54:30 +0100

    added view enapsulation

--- src/app/buttons/square-button/square-button.component.ts
+++ src/app/buttons/square-button/square-button.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-square-button',
   templateUrl: './square-button.component.html',
   styleUrls: ['./square-button.component.less'],
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class SquareButtonComponent implements OnInit {
   @Input()
